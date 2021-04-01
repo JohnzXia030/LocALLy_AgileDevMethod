e77d623f81a6e06a93bdfa1a4eeb20b6745dbd17
Domi <domasanya2@gmail.com>
2021-04-01 18:15:20 +0200

    homepage, header, footer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController{
 
     /**
-     * @Route("/home")
+     * @Route("/home/home")
      */
     public function homeFunction(): Response
     {
