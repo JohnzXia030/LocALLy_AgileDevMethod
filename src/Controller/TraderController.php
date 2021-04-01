5428c22c4856cc25e2f4a1659d94369ca2e6eca8
51924 <johnzx030@gmail.com>
2021-04-01 16:37:46 +0200

    Modif: delete form and its entity; Add trader-create-article

--- src/Controller/TraderController.php
+++ src/Controller/TraderController.php
@@ -4,7 +4,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\CreateShopForm;
@@ -13,16 +17,42 @@ use App\Form\CatalogFormType;
 
 class TraderController extends AbstractController
 {
+    public static $request;
+
+    public function __construct()
+    {
+        $request = new Request();
+        $this::$request = $request;
+    }
+
     /**
      * @Route("/trader/add-article")
+     * @param Connection $connection
+     * @return Response
      */
-    public function addArticle(): Response
+    public function addArticle(Connection $connection): Response
     {
+        /*dump($connection->fetchAll('SELECT * FROM user'));
         $article = new Article();
         $articleForm = $this->createForm(CatalogFormType::class, $article);
-        return $this->render('trader/add-article.html.twig', [
-            'articleForm' => $articleForm->createView()
-        ]);
+        return $this->render('trader/add-article.html.twig', array(
+            'articleForm' => $articleForm->createView(),
+            'test' => $connection->fetchAll('SELECT * FROM user')
+
+        ));*/
+        return $this->render('trader/add-article.html.twig', array());
+    }
+
+    /**
+     * @Route("/trader/api/create-article")
+     * @param ArticleRepository $articleRepository
+     * @return Response
+     */
+    public function apiAddArticle(ArticleRepository $articleRepository): Response
+    {
+        $mNewArticle = json_decode($this::$request->getContent());
+        $result = $articleRepository->addArticle($mNewArticle);
+        return new JsonResponse(['result' => $result]);
     }
 
     /**
