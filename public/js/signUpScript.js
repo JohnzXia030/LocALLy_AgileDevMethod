63a8d8454ccac998b50b20212a0c2ee9ab58a328
OusKing <oussama_hennani@hotmail.fr>
2021-04-06 01:29:19 +0200

    test

--- public/js/signUpScript.js
+++ public/js/signUpScript.js
@@ -1,27 +1,47 @@
 jQuery(document).ready(function(){
-    //console.log("jQuery est prêt !");
+    // code jquery pour la gestion des cliques => a
       var srole = '';
 
       $("#demo2-a").click(function(){
-        srole = 'Professionel';
-        gestionRole(srole);
+        srole = "Professionel";
+        gestionRolePro(srole);
       });
-
+     // code jquery pour la gestion des cliques => b
       $("#demo2-b").click(function(){
-        srole='Particulier';
-        gestionRole(srole);
+        srole="Particulier";
+        gestionRoleParticular(srole);
       });
-
-     /* if(srole="Professionnel"){
-          alert('Vous êtes un ' + srole);
-      }
-
-      if(srole="Particulier"){
-          alert('Vous êtes un ' + srole);
-      }*/
-
 });
 
-function gestionRole(srole){
-    alert("Vous êtes un" + srole); 
+//console.log("Oui");
+
+// Cette fonction gère le contrôle des champs du formulaire
+// 1# en fonction du rôle. 
+function gestionRolePro(srole){
+  //alert("Vous êtes un" + srole)
+  if(srole="Professionnel"){
+    document.getElementById("Inscription").addEventListener("submit", function(){
+      var error;
+      var lastName = document.getElementById("lastName").value;
+
+      if(!lastName.value){
+        error = "Veuillez indiquer votre nom";
+      }
+      if(error){
+        e.preventDefault();
+        document.getElementById("error").innerHTML = erreur; 
+        return false;
+      }
+      else{
+        console.log("formulaire envoyé");
+      }
+    });
+  }
+} 
+
+function gestionRoleParticular(srole){
+  if(srole="Particulier"){
+    console.log("je suis un particulier");
+  }
 }
+  
