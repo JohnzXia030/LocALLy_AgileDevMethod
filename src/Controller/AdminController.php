7e478d839e5ea57bac9d18a244046a9864708aca
OusKing <oussama_hennani@hotmail.fr>
2021-04-27 10:15:22 +0200

    Ajout de la page administrateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController
+
+/**
+ * @Route("/admin")
+ */
+
+class AdminController extends AbstractController
 {
+    /**
+     * @Route("/administrator")
+     */
 
+    public function adminPage(): Response
+    {
+        return $this->render('admin/administrator.html.twig');
+    }
 }
